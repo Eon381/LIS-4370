1398f265f74a256fd37ef1d55086aa1ea061ee3f
Eon381 <frankscollegeemail@gmail.com>
2025-02-03 03:40:57 -0500

    Add files via upload

---
+++ Module #3.R
@@ -0,0 +1,7 @@
+polling_results <- data.frame(
+  Name = c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie"),
+  ABC_political_poll_results = c(4, 62, 51, 21, 2, 14, 15),
+  CBS_political_poll_results = c(12, 75, 43, 19, 1, 21, 19)
+)
+
+polling_results
